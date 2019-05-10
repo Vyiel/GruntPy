d3f496b0e0570ded57c4afd9513b8938f1ec370f
$ git config --global user.name John <budhaditya.blog@gmail.com>
2019-05-10 11:26:39 +0530

    IP Validation Module Completed, IP Subnet Calculation Completed, IP Subnet Segment and Range Calculation Completed, Scapy Successfully Imported and Implemented, Host Discovery and SYN Scan successfully done Visible bugs fixed, termcolor imported and implemented successfully, Successfully exported outputs onto a text file and open it as soon as scan ends. Code Re-Structured Partially : Subnet calculator was ambiguous and was semi automatically coded, finally taken out of main code, coded separated module for subnet calculation only. Testing of Module going on currently. Importing into main code remains. Excess RAM consumption yet to be diagnosed and fixed by re-structuring function calls, dictionaries. etc Excess RAM Consumption is partially being figured out from Scapy Official hub as, 2.4.2 was somehow buggy in windows when thrown in a loop. Re-Structured code in a more modular way by splitting almost every activity in functions and calling it on condition mismatch in place of restarting the whole code by re calling main() which did yield some unknown errors, and now fixed. Added the functions in switch case like dictionary method. One could easily add a function on the scanners.py and add it on the main code in the dictionary items list and throw in a couple of simple code to call it. Saving the file only on demand added successfully, and being opened as soon as being saved. | Coming soon: banner grabbing

--- GruntPy.py
+++ GruntPy.py
@@ -9,9 +9,8 @@ from termcolor import *
 import webbrowser
 import colorama
 colorama.init()
-from memory_profiler import profile
 
-@profile()
+
 def main():
 
     portRange = []
@@ -23,8 +22,60 @@ def main():
     text_out_ping = []
     results = {}
     ip = []
-    bfdict = {}
-    times = int(0)
+    sdict = {}
+
+    def basicFuncSelect():
+        bfdict = {
+            1: bf.preDefinedPorts,
+            2: bf.manualPorts,
+            3: bf.manualRange
+
+        }
+
+        cprint("""
+        1) For scanning with Common Pre Defined Ports!
+        2) For scanning with ports manually entered!
+        3) For scanning with port range entered!
+        """,
+               "blue")
+
+        while True:
+
+            action1 = raw_input("Enter selection: ")
+            try:
+                action1 = int(action1)
+            except:
+                cprint("Unavilable selection detected!!! ", "red")
+                continue
+            if action1 > 3 or action1 < 1:
+                cprint("Unavilable selection detected!!! ", "red")
+                continue
+            else:
+                break
+
+        portRange = bfdict.get(action1, bf.unknownAction)()
+        return portRange
+
+    def save(results, scan_id):
+
+        if len(results) != 0:
+            sys.stdout = open(str(scan_id) + ".txt", "w")
+            for keys in results.keys():
+                print ""
+                print "Open ports or alive host status on IP " + str(keys) + ": "
+                for values in results[keys]:
+                    print values
+            print "______________________ End of Scan Results ______________________ "
+            print ""
+            sys.stdout.close()
+            webbrowser.open(str(scan_id) + ".txt")
+        else:
+            sys.stdout = open(str(scan_id) + ".txt", "w")
+            print ""
+            print "No viable results to display!!! "
+            print "______________________ End of Scan Results ______________________ "
+            sys.stdout.close()
+            webbrowser.open(str(scan_id) + ".txt")
 
 
     cprint("Your Scan ID for text output file is: "+str(scan_id), "red")
@@ -53,74 +104,92 @@ def main():
             cprint("Invalid CIDR detected!!! ", "red")
             continue
 
-
-    bfdict = {
-        1: bf.preDefinedPorts,
-        2: bf.manualPorts,
-        3: bf.manualRange
-
+    sdict = {
+        1: 'synScan',
+        2: 'pingScan',
+        3: 'udpScan'
             }
 
     cprint("""    
-    1) For scanning with Common Pre Defined Ports!
-    2) For scanning with ports manually entered!
-    3) For scanning with port range entered!
+        1) For half connect scan (SYN)
+        2) For scanning alive hosts/host in a range
+        3) For scanning UDP ports
     """,
            "blue")
 
     while True:
 
-        action1 = raw_input("Enter selection: ")
+        action2 = raw_input("Enter selection: ")
         try:
-            action1 = int(action1)
+            action2 = int(action2)
         except:
             cprint("Unavilable selection detected!!! ", "red")
             continue
-        if action1 > 3 or action1 < 1:
+        if action2 > 3 or action2 < 1:
             cprint("Unavilable selection detected!!! ", "red")
             continue
         else:
             break
 
-    portRange = bfdict.get(action1, bf.unknownAction)()
+    selection2 = sdict.get(action2, "False")
 
-    for ips in ipRange:
-        # times = times + 1
-        # if times == 5:
-        #     cprint("resting for 15 seconds!!! ", "green")
-        #     time.sleep(5)
-        #     times = 0
+    if selection2 == 'synScan':
+        portRange = basicFuncSelect()
 
+        for ips in ipRange:
+            scan = sc.synScan(ips, portRange)
+            if len(scan[0]) == 0:
+                cprint("No open ports found on: " + ips, "red")
+            else:
+                cprint("On IP: " + ips, "cyan")
+                text_out_ip = ips
+                text_out_ports = []
+                for open_port in scan[0]:
+                    cprint("Port: " + open_port + " is open ", "green")
+                    text_out_ports.append(open_port)
+                results[text_out_ip] = text_out_ports
 
-        scan = sc.synScan(ips, portRange)
-        if len(scan[0]) == 0:
-            cprint("No open ports found on: " + ips, "red")
-        else:
-            cprint("On IP: " + ips, "cyan")
+    if selection2 == 'pingScan':
+        for ips in ipRange:
             text_out_ip = ips
-            text_out_ports = []
-            for open_port in scan[0]:
-                cprint("Port: " + open_port + " is open ", "green")
-                text_out_ports.append(open_port)
-            results[text_out_ip] = text_out_ports
+            ping = sc.pingScan(ips)
+            text_out_ping = []
+            if ping == 1:
+                cprint(ips + " is up!", "green")
+                status = 'alive'
+                text_out_ping.append(status)
+                results[text_out_ip] = [status]
+            else:
+                cprint(ips + " is down!", "red")
 
+    if selection2 == 'udpScan':
+        portRange = basicFuncSelect()
 
+        for ips in ipRange:
+            scan = sc.udpScan(ips, portRange)
+            if len(scan[0]) == 0:
+                cprint("Closed ports found on: " + ips, "red")
+            else:
+                cprint("On IP: " + ips, "cyan")
+                text_out_ip = ips
+                text_out_ports = []
+                for open_port in scan[0]:
+                    cprint("Port: " + open_port + " is probably open or filtered ", "green")
+                    text_out_ports.append(open_port)
+                results[text_out_ip] = text_out_ports
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    while True:
+        if_save = raw_input("Do you want to save the results into current directory? (y/n) ")
+        if if_save == "y":
+            save(results, scan_id)
+            break
+        elif if_save == "n":
+            cprint("Thank you for using GruntPy. Quitting!!! ", "red")
+            sys.exit()
+            break
+        else:
+            cprint("Invalid selection detected", "red")
+            continue
 
 
 if __name__ == '__main__':
